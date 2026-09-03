72b3ab13c4383c68a001dc3b21436a23f3101917
notSnwyz <mario.fratila081@gmail.com>
2026-09-03 20:17:02 +0100

    Added anime progress and status management and overall cleaned up the code

--- main.py
+++ main.py
@@ -1,31 +1,11 @@
 from models.anime import Anime, AnimeStatus
-import datetime
-
-anime1 = Anime(
-    "NGE",
-    AnimeStatus.PLANNING,
-    0,
-    25,
-    None,
-    None,
-    None
-)
-
-print(anime1.name)
-print(anime1.status.value)
-print(anime1.episode_progress)
-print(anime1.total_episodes)
-print(anime1.start_date)
-print(anime1.end_date)
-print(anime1.rating)
-
 
 anime2 = Anime(
     "Ditf",
-    AnimeStatus.WATCHING,
-    5,
+    AnimeStatus.PLANNING,
+    0,
     24,
-    datetime.date.today(),
+    None,
     None,
     None
 )
@@ -39,8 +19,18 @@ print(anime2.start_date)
 print(anime2.end_date)
 print(anime2.rating)
 
-anime2.update_progress(7)
-anime2.change_status(AnimeStatus.COMPLETED)
+anime2.update_progress(1)
+
+print("\n")
+print(anime2.name)
+print(anime2.status.value)
+print(anime2.episode_progress)
+print(anime2.total_episodes)
+print(anime2.start_date)
+print(anime2.end_date)
+print(anime2.rating)
+
+anime2.update_progress(24)
 
 print("\n")
 print(anime2.name)
--- models/anime.py
+++ models/anime.py
@@ -1,5 +1,5 @@
-from enum import Enum
 import datetime
+from enum import Enum
 
 
 class AnimeStatus(Enum):
@@ -13,16 +13,15 @@ class AnimeStatus(Enum):
 
 class Anime:
     def __init__(
-            self,
-            name: str,
-            status: AnimeStatus,
-            episode_progress: int,
-            total_episodes: int,
-            start_date: datetime.date | None,
-            end_date: datetime.date | None,
-            rating: int | None
-            ):
-
+        self,
+        name: str,
+        status: AnimeStatus,
+        episode_progress: int,
+        total_episodes: int,
+        start_date: datetime.date | None,
+        end_date: datetime.date | None,
+        rating: int | None,
+    ):
         self.name = name
 
         if not isinstance(status, AnimeStatus):
@@ -36,7 +35,9 @@ class Anime:
         self.total_episodes = total_episodes
 
         if episode_progress < 0 or episode_progress > total_episodes:
-            raise ValueError("Episode progress cannot be negative or greater than total episodes")
+            raise ValueError(
+                "Episode progress cannot be negative or greater than total episodes"
+            )
 
         self.episode_progress = episode_progress
 
@@ -48,7 +49,11 @@ class Anime:
         if end_date is not None and not isinstance(end_date, datetime.date):
             raise ValueError("Invalid end date")
 
-        if start_date is not None and end_date is not None and end_date < start_date:
+        if (
+            start_date is not None
+            and end_date is not None
+            and end_date < start_date
+        ):
             raise ValueError("End date cannot be before start date")
 
         self.end_date = end_date
@@ -56,24 +61,24 @@ class Anime:
         if rating is not None and (rating < 0 or rating > 100):
             raise ValueError("Rating must be a number between 0 and 100")
 
-        self.rating = rating  # Rating out of 100
+        self.rating = rating
 
-
-    def update_progress(
-            self,
-            new_progress: int
-            ):
-        
+    def update_progress(self, new_progress: int):
         if new_progress < 0 or new_progress > self.total_episodes:
-            raise ValueError("Episode progress cannot be negative or greater than total episodes")
+            raise ValueError(
+                "Episode progress cannot be negative or greater than total episodes"
+            )
 
         self.episode_progress = new_progress
 
-    def change_status(
-            self,
-            new_status: AnimeStatus
-            ):
+        if self.episode_progress == 1 and self.start_date is None:
+            self.start_date = datetime.date.today()
+            self.change_status(AnimeStatus.WATCHING)
 
+        if self.episode_progress == self.total_episodes:
+            self.change_status(AnimeStatus.COMPLETED)
+
+    def change_status(self, new_status: AnimeStatus):
         if not isinstance(new_status, AnimeStatus):
             raise ValueError("Invalid status")
 
